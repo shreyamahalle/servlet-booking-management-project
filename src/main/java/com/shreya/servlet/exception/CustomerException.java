dde6d0b93606fa9e0100e6f2c99214268b3e07ff
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-02 10:39:35 +0530

    changes added

--- src/main/java/com/shreya/servlet/exception/CustomerException.java
+++ src/main/java/com/shreya/servlet/exception/CustomerException.java
@@ -3,6 +3,7 @@ package com.shreya.servlet.exception;
 
 public class CustomerException extends RuntimeException{
     public CustomerException(String NotAvailable){
+
         super(NotAvailable);
     }
 }
